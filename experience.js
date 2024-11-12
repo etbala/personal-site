45210a6e3a9a1998e0c5a4840666b31047c2fb97
Ethan Balakumar <ethan.balakumar@gmail.com>
2024-11-12 14:21:55 -0500

    File Restructure

--- experience.js
+++
@@ -1,9 +0,0 @@
-// const observer = new IntersectionObserver((entries) => {
-//     entries.forEach(entry => {
-//         if (entry.isIntersecting) {
-//             entry.target.classList.add('show');
-//         }
-//     });
-// });
-
-// document.querySelectorAll('.fade-in').forEach((section) => observer.observe(section));
