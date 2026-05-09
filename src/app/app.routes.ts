dafb2cf32e64790c557fdc05102d34f224c7e7b7
etbala <76022348+etbala@users.noreply.github.com>
2026-05-08 21:48:40 -0500

    new website

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { ResumeComponent } from './pages/resume/resume.component';
 
-export const routes: Routes = [
-    {path: '', component: HomeComponent },
-    {path: 'resume', component: ResumeComponent },
-    {path: '**', redirectTo: '' },
-];
+export const routes: Routes = [];
